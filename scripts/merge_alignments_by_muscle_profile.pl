d4172e8dce2b436cd66ea4f611f66047df261d0c
Wenjie Deng <dengw@uw.edu>
2020-11-18 12:49:45 -0800

    modified merge_alignments_by_muscle_profile.pl to add one option of suffix for the output file

--- scripts/merge_alignments_by_muscle_profile.pl
+++ scripts/merge_alignments_by_muscle_profile.pl
@@ -16,6 +16,7 @@ use File::Basename;
 my %option = (
 	'id1' => '',
 	'id2' => '',
+	'sf'  => '',
 );
 
 my $usage = "\nusage: merge_alignments_by_muscle_profile.pl [-option value]
@@ -23,13 +24,15 @@ my $usage = "\nusage: merge_alignments_by_muscle_profile.pl [-option value]
 options:  
 -id1     input directory of first alignments
 -ld2     input directory of second alignments
+-sf      outfile suffix (including file extension)
 
 ";
 
-GetOptions (\%option, 'id1=s', 'id2=s');
+GetOptions (\%option, 'id1=s', 'id2=s', 'sf=s');
 
 my $indir1 = $option{'id1'} or die $usage;
 my $indir2 = $option{'id2'} or die $usage;
+my $suffix = $option{'sf'} or die $usage;
 my $count = 0;
 my (@filenames, %idfilename);
 opendir DIR, $indir1 or die "couldn't open $indir1: $!\n";
@@ -42,7 +45,7 @@ closedir DIR;
 
 opendir DIR, $indir2 or die "couldn't open $indir2: $!\n";
 while (my $file = readdir DIR) {
-	if ($file =~ /_collapse.fasta$/) {
+	if ($file =~ /\.fasta$/) {
         my @fields = split /_/, $file;
         my $id = $fields[0]."_".$fields[1];
         $idfilename{$id} = $file;
@@ -63,13 +66,13 @@ foreach my $filename (@filenames) {
         my $region2 = $parts[3];
         if ($region1 eq $region2) {
             print "\n=== Profile-profile alignment for $firstfile and $secondfile ===\n";
-            my $outfile = $id."_".$firstTP."-".$secondTP."_".$region1."_NT_1stTPUncollapsed.fasta";
+            my $outfile = $id."_".$firstTP."-".$secondTP."_".$region1."_".$suffix;
             system("muscle -profile -quiet -in1 $firstfile -in2 $secondfile -out $outfile");
         }else {
             die "region is different: $region1 vs. $region2\n";
         }      
     }else {
-        die "No corresponding second alignment for $filename\n";
+        print "No corresponding second alignment for $filename\n";
     }
     
 }
